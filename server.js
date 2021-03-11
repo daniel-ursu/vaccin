74292b237af7e63afdce2e7a0b3df2c9d3b9f8e0
danielu <daniel@ursu.biz>
2021-03-11 12:24:40 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+const fetch = require("node-fetch");
+
+var pages = [];
+for (var i = 1; i <= 25; i++) {
+  pages.push(i);
+}
+
+
+for (var page of pages) {
+  fetch("https://programare.vaccinare-covid.gov.ro/scheduling/api/centres?page=" + page + "&size=20&sort=,", {
+    "headers": {
+      "accept": "application/json, text/plain, */*",
+      "accept-language": "en-US,en;q=0.9,ro;q=0.8",
+      "content-type": "application/json",
+      "sec-ch-ua": "\"Chromium\";v=\"88\", \"Google Chrome\";v=\"88\", \";Not A Brand\";v=\"99\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin",
+      "cookie": "SESSION=FILL THIS"
+    },
+    "referrer": "https://programare.vaccinare-covid.gov.ro/",
+    "referrerPolicy": "strict-origin-when-cross-origin",
+    "body": "{\"countyID\":null,\"localityID\":null,\"name\":null,\"identificationCode\":\"1821005012654\",\"masterPersonnelCategoryID\":-4,\"personnelCategoryID\":32,\"recipientID\":4186562}",
+    "method": "POST",
+    "mode": "cors"
+  }).then(res => res.json())
+    .then(json => {
+      var content = json.content;
+      for (var c of content) {
+        if (c.availableSlots > 0) {
+          console.log(c);
+        }
+      }
+    })
+}
